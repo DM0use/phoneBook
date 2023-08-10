95de1836a3083f5098ce8c584d8ffa4f6d53ba88
James Blakely <jblakely90@gmail.com>
2023-08-11 01:11:29 +1200

    exercise 3.8 done

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(express.json())
+app.use(requestLogger)
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+      tokens.method(req) === 'POST' ? JSON.stringify(req.body) : '',
+    ].join(' ')
+  })
+)
 
 let persons = [
   {
@@ -84,6 +109,12 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
